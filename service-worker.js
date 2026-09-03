91e2d6324afd7f787e6a1ef9ec3f737edb90a912
nutricionistabrunaribeiro <nutricionistabrunaribeiro@gmail.com>
2026-09-03 19:01:55 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'meu-ecossistema-v10';
+const CACHE_NAME = 'meu-ecossistema-v11';
 const APP_SHELL = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', event => {
